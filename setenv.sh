a6c6c719f7ff6e01a94be20499f0073f4d4bdb1a
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2024-04-20 21:37:12 +0200

    scripts

--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,9 @@
-py312
+export ImageBrowser_source_path=${PWD}
 
-append_to_python_path_if_not ${PWD}
-append_to_python_path_if_not ${PWD}/tools
+# source /opt/python-virtual-env/py312/bin/activate
+source /opt/python-virtual-env/qt/bin/activate
+
+export PYTHONPYCACHEPREFIX=${ImageBrowser_source_path}/.pycache
+export PYTHONDONTWRITEBYTECODE=${ImageBrowser_source_path}/.pycache
+
+append_to_python_path_if_not ${ImageBrowser_source_path}
