1fa46c82c57e41453b44498d4b4d61a0549b457e
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2024-04-24 23:10:39 +0200

    spdx

--- ImageBrowser/frontend/KeySequenceEditor.py
+++ ImageBrowser/frontend/KeySequenceEditor.py
@@ -9,26 +9,9 @@
 ####################################################################################################
 #
 #  Translated from C++ to Python
-#
-#   https://github.com/mitchcurtis/slate/blob/master/lib/keysequenceeditor.cpp
-#   https://github.com/mitchcurtis/slate/blob/master/lib/keysequenceeditor.h
-#
-#     Copyright 2016, Mitch Curtis
-#
-#     This file is part of Slate.
-#
-#     Slate is free software: you can redistribute it and/or modify
-#     it under the terms of the GNU General Public License as published by
-#     the Free Software Foundation, either version 3 of the License, or
-#     (at your option) any later version.
-#
-#     Slate is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU General Public License for more details.
-#
-#     You should have received a copy of the GNU General Public License
-#     along with Slate. If not, see <http://www.gnu.org/licenses/>.
+#    https://github.com/mitchcurtis/slate/blob/master/lib/keysequenceeditor.cpp
+#    https://github.com/mitchcurtis/slate/blob/master/lib/keysequenceeditor.h
+#    Copyright 2016, Mitch Curtis GPL-3.0-or-later
 #
 ####################################################################################################
 
