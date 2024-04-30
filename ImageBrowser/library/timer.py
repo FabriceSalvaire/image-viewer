69ee3c92585ab4a67920ea58cff85405d627fa49
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2024-04-30 16:01:44 +0200

    timer

--- ImageBrowser/library/timer.py
+++ ImageBrowser/library/timer.py
@@ -15,6 +15,7 @@ __all__ = ['Timer', 'TimerNs', 'TimerManager']
 
 ####################################################################################################
 
+from datetime import timedelta
 from time import time_ns, perf_counter, perf_counter_ns
 
 ####################################################################################################
@@ -25,12 +26,41 @@ class TimerMixin:
 
     ##############################################
 
+    # Fixme: overflow
+
+    @staticmethod
+    def ns2us(x: int | float) -> float:
+        return x / 1000
+
+    @staticmethod
+    def ns2ms(x: int | float) -> float:
+        return x / 1000_000
+
+    @staticmethod
+    def ns2s(x: int | float) -> float:
+        return x / 1000_000_000
+
+    @staticmethod
+    def s2ms(x: int | float) -> float:
+        return x * 1000
+
+    @staticmethod
+    def s2us(x: int | float) -> float:
+        return x * 1000_000
+
+    @staticmethod
+    def s2ns(x: int | float) -> float:
+        return x * 1000_000_000
+
+    ##############################################
+
     def __init__(self, name: str, start: bool = True) -> None:
         self._name = str(name)
         self._start = None
         self._stop = None
+        self._accumulator = 0
         if start:
-            self.reset()
+            self.start()
 
     ##############################################
 
@@ -38,9 +68,36 @@ class TimerMixin:
     def name(self) -> str:
         return self._name
 
+    ##############################################
+
+    @property
+    def raw_delta(self) -> int | float:
+        if self._stop is not None:
+            return self._stop - self._start
+        return None
+
+    @property
+    def delta(self) -> timedelta:
+        return timedelta(milliseconds=self.delta_ms)
+
+    ##############################################
+
+    def accumulate(self) -> None:
+        self._accumulator += self.raw_delta
+
+    @property
+    def raw_accumulator(self) -> int | float:
+        return self._accumulator
+
+    @property
+    def accumulator(self) -> timedelta:
+        return timedelta(milliseconds=self.accumulator_ms)
+
 ####################################################################################################
 
 class TimerNs(TimerMixin):
+
+    # Fixme: overflow, run perf_counter in parallel to detect that ?
 
     ##############################################
 
@@ -55,17 +112,37 @@ class TimerNs(TimerMixin):
 
     @property
     def delta_ns(self) -> int:
-        if self._stop is not None:
-            return self._stop - self._start
-        return None
+        return self.raw_delta
+
+    @property
+    def delta_us(self) -> float:
+        return self.ns2us(self.raw_delta)
 
     @property
     def delta_ms(self) -> float:
-        return self.delta_ns / 1000
+        return self.ns2ms(self.raw_delta)
 
     @property
     def delta_s(self) -> float:
-        return self.delta_ns / 1000_000
+        return self.ns2s(self.raw_delta)
+
+    ##############################################
+
+    @property
+    def accumulator_ns(self) -> int:
+        return self.raw_accumulator
+
+    @property
+    def accumulator_us(self) -> float:
+        return self.ns2us(self.raw_accumulator)
+
+    @property
+    def accumulator_ms(self) -> float:
+        return self.ns2ms(self.raw_accumulator)
+
+    @property
+    def accumulator_s(self) -> float:
+        return self.ns2s(self.raw_accumulator)
 
 ####################################################################################################
 
@@ -84,13 +161,29 @@ class Timer(TimerMixin):
 
     @property
     def delta_s(self) -> float:
-        if self._stop is not None:
-            return self._stop - self._start
-        return None
+        return self.raw_delta
 
     @property
     def delta_ms(self) -> float:
-        return self.delta_s * 1000
+        return self.s2ms(self.raw_delta)
+
+    @property
+    def delta_us(self) -> float:
+        return self.s2us(self.raw_delta)
+
+    ##############################################
+
+    @property
+    def accumulator_s(self) -> float:
+        return self.raw_accumulator
+
+    @property
+    def accumulator_ms(self) -> float:
+        return self.s2ms(self.raw_accumulator)
+
+    @property
+    def accumulator_us(self) -> float:
+        return self.s2us(self.raw_accumulator)
 
 ####################################################################################################
 
